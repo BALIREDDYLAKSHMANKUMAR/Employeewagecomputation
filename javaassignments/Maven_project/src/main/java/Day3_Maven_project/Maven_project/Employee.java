e9ce8cd51773a603e1f16b6612ed6f0cdbd90bab
lakshmankumar <laxmansgvp@gmail.com>
2022-09-18 12:12:26 +0530

    adding employee attendencee

--- javaassignments/Maven_project/src/main/java/Day3_Maven_project/Maven_project/Employee.java
+++ javaassignments/Maven_project/src/main/java/Day3_Maven_project/Maven_project/Employee.java
@@ -3,7 +3,15 @@ package Day3_Maven_project.Maven_project;
 public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Employee Wage Computation");
+		int IsFullTime =1;
+		double empcheck = Math.floor(Math.random()*10)%2;
+		if (empcheck == IsFullTime) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is abscent");
 		
+		}
 	}
 	
 
