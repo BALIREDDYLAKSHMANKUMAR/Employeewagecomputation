e072507492a33471839eed708bf6fd1f5f1b466b
lakshmankumar <laxmansgvp@gmail.com>
2022-09-18 13:00:17 +0530

    adding EmployeeSwitchCase methode

--- javaassignments/Maven_project/src/main/java/Day3_Maven_project/Maven_project/Employee.java
+++ javaassignments/Maven_project/src/main/java/Day3_Maven_project/Maven_project/Employee.java
@@ -1,24 +1,31 @@
 package Day3_Maven_project.Maven_project;
 
 public class Employee {
-	public static void main(String[] args) {
-		int IsFullTime =1;
-		int IsPartTime =2;
-		int EmpRatePerHour=20;
-		int emphrs=0;
-		int empwage=0;
-		double empcheck=Math.floor(Math.random()*10)%3;
-		if (empcheck == IsFullTime)
-			emphrs=8;
-		else if (empcheck == IsPartTime)
-			emphrs=4;
-		else
-			emphrs=0;
-		empwage = emphrs*EmpRatePerHour;
-		System.out.println("EmpWage:"+empwage);
-	
-	}
-	}
-	
+	final static int IsFullTime = 1;
+    final static int IspartTime=2;
+    final static int EmpRatePerHour=20;
 
-
+public static void main(String[] args) {
+	int emphrs=0;
+	int empwage=0;
+	int empcheck= (int)Math.floor(Math.random()*10)%3;
+	switch(empcheck) {
+	case IsFullTime:
+		emphrs=8;
+		break;
+	case IspartTime:
+		emphrs=4;
+		break;
+	default:
+		emphrs=0;
+		
+	}
+	empwage = emphrs * EmpRatePerHour;
+	System.out.println("EmpWage:"+empwage);
+}
+	
+	
+			
+			
+		
+}
