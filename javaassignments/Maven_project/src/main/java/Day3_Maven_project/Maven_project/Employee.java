f8a1919511a1dad98a391d4343f7ef32cd99887f
lakshmankumar <laxmansgvp@gmail.com>
2022-09-18 13:07:05 +0530

    adding Employee total time and Wage

--- javaassignments/Maven_project/src/main/java/Day3_Maven_project/Maven_project/Employee.java
+++ javaassignments/Maven_project/src/main/java/Day3_Maven_project/Maven_project/Employee.java
@@ -1,31 +1,38 @@
 package Day3_Maven_project.Maven_project;
 
 public class Employee {
-	final static int IsFullTime = 1;
-    final static int IspartTime=2;
-    final static int EmpRatePerHour=20;
-
-public static void main(String[] args) {
-	int emphrs=0;
-	int empwage=0;
-	int empcheck= (int)Math.floor(Math.random()*10)%3;
-	switch(empcheck) {
-	case IsFullTime:
-		emphrs=8;
-		break;
-	case IspartTime:
-		emphrs=4;
-		break;
-	default:
-		emphrs=0;
-		
+	public static final int IsFullTime = 1;
+	public static final int IsPartTime = 2;
+	public static final int EmpRatePerHour = 20;
+	public static final int NumOfWorkingDays = 20;
+	public static final int MaxHrsInMonth = 10;
+	public static void main(String[] args) {
+		int emphrs =0,totalEmphrs=0,totalWorkingDays=0;
+		while(totalEmphrs <= MaxHrsInMonth && totalWorkingDays <NumOfWorkingDays) {
+			totalWorkingDays ++;
+			int empcheck = (int)Math.floor(Math.random()*10)%3;
+			switch (empcheck){
+			case IsFullTime:
+				emphrs =8;
+				break;
+			case IsPartTime:
+				emphrs =4;
+				break;
+			default:
+				emphrs = 0;
+				
+			}
+			totalEmphrs += emphrs;
+			System.out.println("day#"+totalWorkingDays+"emphrs:"+emphrs);
+		}
+		int totalEmpWage = totalEmphrs*EmpRatePerHour;
+		System.out.println("Total Emp Wage:"+totalEmpWage);
 	}
-	empwage = emphrs * EmpRatePerHour;
-	System.out.println("EmpWage:"+empwage);
+	
 }
 	
 	
 			
 			
 		
-}
+
